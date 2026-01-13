591813dc898990cf19b4f8e2fcf5c0830e4b206c
Duc Thang Nguyen <Nickworkspace189@gmail.com>
2026-01-13 15:09:26 +0700

    update with typescripts for simulation service

---
+++ gen/ts/index.ts
@@ -0,0 +1,10 @@
+export * from './identity/v1/user_connect';
+export * from './identity/v1/user_pb'; // Usually the message definitions
+
+export * from './common/v1/common_pb';
+
+export * from './ride/v1/ride_connect';
+export * from './ride/v1/ride_pb';
+
+export * from './tracking/v1/tracking_connect';
+export * from './tracking/v1/tracking_pb';
