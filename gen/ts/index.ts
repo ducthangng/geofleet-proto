f53d8439a6f6e7cb6aeb8fdbb75f651f457296c6
Duc Thang Nguyen <Nickworkspace189@gmail.com>
2026-01-13 20:25:03 +0700

    update index.ts

--- gen/ts/index.ts
+++ gen/ts/index.ts
@@ -1,10 +1,4 @@
-export * from './identity/v1/user_connect.js';
-export * from './identity/v1/user_pb.js'; // Usually the message definitions
-
+export * from './identity/v1/user_pb.js';
 export * from './common/v1/common_pb.js';
-
-export * from './ride/v1/ride_connect.js';
 export * from './ride/v1/ride_pb.js';
-
-export * from './tracking/v1/tracking_connect.js';
 export * from './tracking/v1/tracking_pb.js';
